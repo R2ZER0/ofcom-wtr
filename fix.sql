aa600270b2850e5478f5c2e8018c2d1ecc1605dd
Rikki Guy <r.guy@mhnltd.co.uk>
2018-02-22 01:28:32 +0000

    Add actually copy/import command

--- fix.sql
+++ fix.sql
@@ -1,3 +1,5 @@
+copy wtr ("Lic No", "Licensee", "Product","Publish_Flag","Trade_Flag","Issue Date", "lic status", "Latitiude", "Longitude", "NGR", "TX/Rx", "Freq", "Channel_Width", "Height", "Max Radiated Power ERP", "Antenna Type", "Gain", "Azimuth", "An Code H", "An Code V", "Antenna Height", "Antenna Location", "EFL_UPPER_LOWER", "Ant Elevation", "Ant Polarisation", "Antenna Name", "Ant Feeding Loss", "Fade Margin", "EMI Code", "Area Code/Country", "thing") FROM '/data/WTR-Register-Report-12012018.csv' csv header delimiter ',' quote '"';
+
 update wtr set "Area Code/Country" = "thing" where thing<>'';
 update wtr set thing=NULL;
 
